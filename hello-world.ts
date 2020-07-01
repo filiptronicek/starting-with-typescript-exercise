bdcd27545209b462e6724321b5c246ab036a0a8b
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-01 17:07:55 +0200

    Add dates

--- hello-world.ts
+++ hello-world.ts
@@ -1,4 +1,9 @@
 function doIt(msg: string): void {
     const theText = msg;
     alert(theText);
+
+    const datenow = new Date();
+    const hour:number = datenow.getHours();
+
+    console.log(hour);
 }
